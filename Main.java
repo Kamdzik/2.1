a3fcd950beedf0958ac81f3221d6f0cb32cdebc6
Kamdzik <127319303+Kamdzik@users.noreply.github.com>
2023-03-22 13:37:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+// System.out.println("Hello world!");
+ String napis = "Hello world!";
+  
+for(int a=0; a<11; a++){
+   System.out.println(napis.charAt(a));
+  
+    }  
+  }
+}
